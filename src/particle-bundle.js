3fd3b5e409971d00de806871ba60e618d101146c
ics-nohara <nozomi.nohara@ics-web.jp>
2016-02-23 19:15:53 +0900

    ソース上のjsを削除

--- src/particle-bundle.js
+++
@@ -1,6 +0,0 @@
-/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts"/>
-/// <reference path="../typings/main.d.ts"/>
-var particle_emitter_1 = require("./particle/particle-emitter");
-window.effects = window.effects || {};
-window.effects.ParticleEmitter = particle_emitter_1.ParticleEmitter;
-//# sourceMappingURL=particle-bundle.js.map
