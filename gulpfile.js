d9a0322ec4c1fb3a172aa4c0acb0469d70df737b
ics-ikeda <ikeda@ics-web.jp>
2018-04-17 14:13:56 +0900

    Gulpfileをコードフォーマット

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,55 @@
 var gulp = require("gulp");
-var shell = require('gulp-shell');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var del = require('del');
-var runSequence = require('run-sequence');
+var shell = require("gulp-shell");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var del = require("del");
+var runSequence = require("run-sequence");
 
-gulp.task("copy", function () {
+gulp.task("copy", function() {
   // コピー元フォルダーの指定
-  gulp.src("../core/libs/asset-shapes.js")
+  gulp
+    .src("../core/libs/asset-shapes.js")
     // コピー先フォルダーの指定
     .pipe(gulp.dest("./"));
 });
 
 gulp.task("default", ["copy"]);
 
-gulp.task('concat', function () {
-  gulp.src(['particlejs.js.tmp', 'src/asset-shapes.js'])
-    .pipe(concat('libs/particlejs.js'))
-    .pipe(gulp.dest('./'));
+gulp.task("concat", function() {
+  gulp
+    .src(["particlejs.js.tmp", "src/asset-shapes.js"])
+    .pipe(concat("libs/particlejs.js"))
+    .pipe(gulp.dest("./"));
 });
 
-
-gulp.task("uglify", shell.task([
+gulp.task(
+  "uglify",
+  shell.task([
     "uglifyjs --compress --mangle -- libs/particlejs.js > libs/particlejs.min.js"
   ])
 );
 
-
-gulp.task("build-particle-system", shell.task([
+gulp.task(
+  "build-particle-system",
+  shell.task([
     "tsc -p src --outDir tmp --module commonjs --declaration",
     "browserify tmp/particle-bundle.js > particlejs.js.tmp"
   ])
 );
 
-gulp.task('clean-tmp', function (cb) {
-  del(['particlejs.js.tmp', 'tmp'], cb);
+gulp.task("clean-tmp", function(cb) {
+  del(["particlejs.js.tmp", "tmp"], cb);
 });
 
-gulp.task("start", function () {
+gulp.task("start", function() {
   return runSequence("build-particle-system", "concat", "uglify", "clean-tmp");
 });
 
 var typedoc = require("gulp-typedoc");
 
-gulp.task("typedoc", function () {
-  return gulp
-    .src(["libs/particlejs.d.ts"])
-    .pipe(typedoc({
+gulp.task("typedoc", function() {
+  return gulp.src(["libs/particlejs.d.ts"]).pipe(
+    typedoc({
       // TypeScript options (see typescript docs)
       module: "commonjs",
       target: "es5",
@@ -61,7 +64,7 @@ gulp.task("typedoc", function () {
       name: "ParticleJS",
       theme: "minimal",
       ignoreCompilerErrors: true,
-      version: true,
-    }))
-    ;
+      version: true
+    })
+  );
 });
